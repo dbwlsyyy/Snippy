c49bbb7472ee529ec2ec0be1e3bebe9caf54983d
홍유진 <dbwlsyyy@naver.com>
2025-08-05 00:55:00 +0900

    style: rem 단위 기준 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import styles from './App.module.css';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import styles from './SideBar.module.css';
 
 export default function SideBar() {
