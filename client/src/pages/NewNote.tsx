388062af1f89578afc1bc871cef56e8462beb8d4
홍유진 <dbwlsyyy@naver.com>
2025-08-06 04:51:21 +0900

    feat: new note 컴포넌트 페이지 구현

--- client/src/pages/NewNote.tsx
+++ client/src/pages/NewNote.tsx
@@ -5,8 +5,8 @@ export default function NewNote() {
     const [mode, setMode] = useState<'note' | 'snippet'>('note');
     const isNoteMode = mode === 'note';
 
-    const [title, setTitle] = useState<string>('제목을 입력해주세요.');
-    const [desc, setDesc] = useState<string>('간단 설명을 작성해주세요.');
+    const [title, setTitle] = useState<string>('');
+    const [desc, setDesc] = useState<string>('');
 
     const [tags, setTags] = useState<string[]>([]);
     const [noteContent, setNoteContent] = useState<string>('');
@@ -16,19 +16,44 @@ export default function NewNote() {
     };
 
     return (
-        <div className={styles.noteFormContainer}>
-            <div className={styles.FormHeader}>
-                <h2>새 {isNoteMode ? '노트' : '스니펫'} 작성</h2>
-                <button onClick={handleMode} className={styles.btnMode}>
-                    {isNoteMode ? '스니펫 모드로 전환' : '노트 모드로 전환'}
-                </button>
+        <div className={styles.container}>
+            <div className={styles.header}>
+                <div className={styles.headerNumber}>
+                    <h2># 1</h2>
+                    <button onClick={handleMode} className={styles.btnMode}>
+                        {isNoteMode ? '스니펫 모드로 전환' : '노트 모드로 전환'}
+                    </button>
+                </div>
+
+                <textarea
+                    className={styles.headerTitle}
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    placeholder="제목을 입력해주세요."
+                    maxLength={50}
+                />
+
+                <div className={styles.headerDescription}>
+                    <h3>Description/</h3>
+                    <input
+                        className={styles.descInput}
+                        type="text"
+                        value={desc}
+                        onChange={(e) => setDesc(e.target.value)}
+                        placeholder="노트 내용 한 줄로 요약해보셈"
+                    />
+                </div>
             </div>
+
             <hr />
 
-            <div>
-                <h2>Title: {title}</h2>
-                <h3>Description: {desc}</h3>
-                <textarea></textarea>
+            <div className={styles.contents}>
+                <textarea
+                    className={styles.contentsInput}
+                    value={noteContent}
+                    onChange={(e) => setNoteContent(e.target.value)}
+                    placeholder="오늘의 아이디어를 기록하세요."
+                ></textarea>
             </div>
         </div>
     );
