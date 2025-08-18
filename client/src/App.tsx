6639abc32de94429ccf08edb837edcdb5356642e
홍유진 <dbwlsyyy@naver.com>
2025-08-19 00:08:46 +0900

    feat: 사이드바 접기 기능 구현 마무리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,26 @@ import styles from './App.module.css';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
 import AppRoutes from './routes/AppRoutes';
+import { useState } from 'react';
 
 function App() {
     // 왜 브라우저 라우터 위치를 여기에?
+    const [isFolded, setIsFolded] = useState<boolean>(false);
+    const sidebarWidth = isFolded ? '5rem' : '25rem';
     return (
         <BrowserRouter>
             <div className={styles.appContainer}>
                 <Header />
 
-                <div className={styles.mainContentArea}>
-                    <SideBar />
+                <div
+                    className={styles.mainContentArea}
+                    style={
+                        {
+                            '--sidebar-current-width': sidebarWidth,
+                        } as React.CSSProperties
+                    }
+                >
+                    <SideBar isFolded={isFolded} setIsFolded={setIsFolded} />
                     <main className={styles.mainSection}>
                         <AppRoutes />
                     </main>
--- client/src/components/SideBar.tsx
+++ client/src/components/SideBar.tsx
@@ -9,11 +9,14 @@ import {
     RiSettings3Line,
 } from 'react-icons/ri';
 
-import { useState } from 'react';
+interface SideBarProps {
+    isFolded: boolean;
+    setIsFolded: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
-export default function SideBar() {
+export default function SideBar({ isFolded, setIsFolded }: SideBarProps) {
     const navigate = useNavigate();
-    const [isFolded, setIsFolded] = useState<boolean>(false);
+    const svgSize = isFolded ? '2.3rem' : '2rem';
 
     interface SidebarItemProps {
         icon: React.ReactNode;
@@ -28,43 +31,44 @@ export default function SideBar() {
                     className={styles.sidebarNavLink}
                     onClick={() => navigate(to)}
                 >
-                    {icon} {label}
+                    {icon}
+                    <span className={styles.label}>{!isFolded && label}</span>
                 </span>
             </li>
         );
     };
 
     return (
-        <nav className={styles.sidebar}>
-            <div
-                className={styles.btnClose}
+        <nav className={`${styles.sidebar} ${isFolded ? styles.folded : ''}`}>
+            <button
+                className={styles.btnToggle}
                 onClick={() => setIsFolded((prev) => !prev)}
             >
-                <TbArrowBarLeft />
-            </div>
+                {isFolded ? <TbArrowBarRight /> : <TbArrowBarLeft />}
+            </button>
             <ul className={styles.sidebarNavList}>
                 <ItemComponent
-                    icon={<RiStickyNoteLine />}
+                    icon={<RiStickyNoteLine size={svgSize} />}
                     label="내 노트"
                     to="/notes"
                 />
                 <ItemComponent
-                    icon={<RiCodeBlock />}
+                    icon={<RiCodeBlock size={svgSize} />}
                     label="내 스니펫"
                     to="/snippets"
                 />
                 <ItemComponent
-                    icon={<RiStarLine />}
+                    icon={<RiStarLine size={svgSize} />}
                     label="즐겨찾기"
                     to="/likes"
                 />
                 <ItemComponent
-                    icon={<RiPriceTag3Line />}
+                    icon={<RiPriceTag3Line size={svgSize} />}
                     label="태그 관리"
                     to="/tags"
                 />
                 <ItemComponent
-                    icon={<RiSettings3Line />}
+                    icon={<RiSettings3Line size={svgSize} />}
                     label="세팅"
                     to="/settings"
                 />
@@ -73,7 +77,10 @@ export default function SideBar() {
                 className={styles.btnNewNote}
                 onClick={() => navigate('/new')}
             >
-                ➕ 새 노트 / 스니펫 작성
+                ➕
+                <span className={styles.label}>
+                    {!isFolded && '새 노트 / 스니펫 작성'}
+                </span>
             </button>
         </nav>
     );
